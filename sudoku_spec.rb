2cf042d21aaad4686fd11a50d23efe1bc8f18e5d
Ryan Jones <run0391@gmail.com>
2015-01-27 15:52:41 -0700

    added test for a guess

--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -18,18 +18,33 @@ describe Sudoku do
   end
 
   it '#row returns a row selected' do
-    expect(game.row(2)).to eq(["2", "-", "-", "4", "-", "-", "8", "1", "9"])
+    expect(game.get_row(2)).to eq(["2", "-", "-", "4", "-", "-", "8", "1", "9"])
   end
 
   it '#column returns a selected column' do
-    expect(game.column(3)).to eq(["8","-","4","-","-","-","6","-","3"])
+    expect(game.get_column(3)).to eq(["8","-","4","-","-","-","6","-","3"])
   end
 
   it '#grid returns a selected grid' do
-    expect(game.grid(4,5)).to eq(["-", "-", "7","-", "8", "3","-", "6", "1"])
+    expect(game.get_grid(4,5)).to eq(["-", "-", "7","-", "8", "3","-", "6", "1"])
   end
 
   it 'returns a list of possible numbers for a given cell' do
-    expect(game.possiblities(2,8)).to eq([1])
+    expect(game.possibilities(8,2)).to eq(["1"])
+  end
+
+  it 'placs a guess in an empty cell' do
+    game.guess
+    expect(game.board).to eq([
+      ["1", "4", "5", "8", "-", "2", "-", "-", "-"],
+      ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+      ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+      ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+      ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+      ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+      ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+      ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+      ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+    ])
   end
 end
